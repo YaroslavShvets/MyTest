4cc2710f791c1c9e4db3fae3984e31e8521dd076
YaroslavShvets <65620471+YaroslavShvets@users.noreply.github.com>
2020-05-19 21:37:34 +0300

    Add files via upload

---
+++ MyTest/MyTest/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5N = new System.Windows.Forms.Label();
+            this.label6G = new System.Windows.Forms.Label();
+            this.label7K = new System.Windows.Forms.Label();
+            this.textBox1N = new System.Windows.Forms.TextBox();
+            this.textBox2G = new System.Windows.Forms.TextBox();
+            this.textBox3K = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(417, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(29, 268);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(101, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(29, 295);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(29, 323);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 20);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(512, 83);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(26, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(29, 349);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(101, 20);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Location = new System.Drawing.Point(284, 377);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(14, 13);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(304, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(330, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(180, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Залишилося";
+            // 
+            // label5N
+            // 
+            this.label5N.AutoSize = true;
+            this.label5N.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5N.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5N.Location = new System.Drawing.Point(26, 134);
+            this.label5N.Name = "label5N";
+            this.label5N.Size = new System.Drawing.Size(106, 18);
+            this.label5N.TabIndex = 12;
+            this.label5N.Text = "Прізвище, Ім\'я";
+            // 
+            // label6G
+            // 
+            this.label6G.AutoSize = true;
+            this.label6G.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6G.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label6G.Location = new System.Drawing.Point(26, 179);
+            this.label6G.Name = "label6G";
+            this.label6G.Size = new System.Drawing.Size(48, 18);
+            this.label6G.TabIndex = 13;
+            this.label6G.Text = "Група";
+            // 
+            // label7K
+            // 
+            this.label7K.AutoSize = true;
+            this.label7K.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7K.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label7K.Location = new System.Drawing.Point(26, 224);
+            this.label7K.Name = "label7K";
+            this.label7K.Size = new System.Drawing.Size(41, 18);
+            this.label7K.TabIndex = 14;
+            this.label7K.Text = "Курс";
+            // 
+            // textBox1N
+            // 
+            this.textBox1N.Location = new System.Drawing.Point(26, 155);
+            this.textBox1N.Name = "textBox1N";
+            this.textBox1N.Size = new System.Drawing.Size(170, 21);
+            this.textBox1N.TabIndex = 15;
+            // 
+            // textBox2G
+            // 
+            this.textBox2G.Location = new System.Drawing.Point(26, 200);
+            this.textBox2G.Name = "textBox2G";
+            this.textBox2G.Size = new System.Drawing.Size(170, 21);
+            this.textBox2G.TabIndex = 16;
+            // 
+            // textBox3K
+            // 
+            this.textBox3K.Location = new System.Drawing.Point(26, 245);
+            this.textBox3K.Name = "textBox3K";
+            this.textBox3K.Size = new System.Drawing.Size(170, 21);
+            this.textBox3K.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 401);
+            this.Controls.Add(this.textBox3K);
+            this.Controls.Add(this.textBox2G);
+            this.Controls.Add(this.textBox1N);
+            this.Controls.Add(this.label7K);
+            this.Controls.Add(this.label6G);
+            this.Controls.Add(this.label5N);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5N;
+        private System.Windows.Forms.Label label6G;
+        private System.Windows.Forms.Label label7K;
+        private System.Windows.Forms.TextBox textBox1N;
+        private System.Windows.Forms.TextBox textBox2G;
+        private System.Windows.Forms.TextBox textBox3K;
+    }
+}
+
---
+++ MyTest/MyTest/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string path; //шлях до файлу
+        string name; 
+
+        
+        System.Xml.XmlReader xmlReader; // XmlReader для читання текстового файлу
+
+        string quest;     // змінна для питання
+        
+        
+        string[] answer = new string[4];  // масив для відповідей (радіобаттонів)
+        string n, g, k, d;// змінні для текст боксів
+        string pic;    // шлях до картинки
+        int m, s;  // змінні для таймеру
+        int right; // номер правильного варіанту відповіді
+        int choosequ;   // номер вибраного студентом варіанту відповіді
+        int sumrig;     // кількість правильних відповідей
+        int sumque;    // загальна кількість питань
+        int mode;  // стан програми:
+                           // 0 - початок роботи авторизація;
+                           // 1 - тестування;
+                           // 2 - кінець
+
+        
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            label2.Visible = false; //непотрібні на початку роботи елементи переведені в невидимий режим
+            label3.Visible = false;
+            label4.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+            radioButton4.Visible = false;
+            pictureBox1.Visible = false;
+            
+            if (args.Length > 0)// вказуємо xml файл як параметр команди запуска
+            {
+              
+            if (args[0].IndexOf(":") == -1)  // вказано тільки ім'я файлу
+            {
+               path = Application.StartupPath + "\\";
+               name = args[0];
+            }               
+            else
+            {
+                // вказаний шлях
+                path = args[0].Substring(0,args[0].LastIndexOf("C:\\Users\\Yaroslav\\Desktop\\Новая папка (2)\\MyTest\\MyTest\\bin\\Debug") +1);
+                name = args[0].Substring(args[0].LastIndexOf("questions.xml")+1);
+            }          
+            xmlReader = new System.Xml.XmlTextReader(path + name);  // отримуємо доступ к xml документу
+            xmlReader.Read();
+
+            mode = 0; // режим програми
+            sumrig   = 0;           
+            this.showHead(); // завантажуємо заголовок                
+            this.showDescription(); // та опис
+            }
+        }
+
+      // функція відображення назви тесту
+      private void showHead()
+      {
+    
+        do xmlReader.Read();             // шукаємо <head>
+        while(xmlReader.Name != "head");    
+        xmlReader.Read();                // читаємо заголовок    
+        this.Text = xmlReader.Value;     // виводимо назву в заголовок    
+        xmlReader.Read();                // виходимо з  <head>    
+
+      }
+   
+
+    // функція для обробки питань
+    private void showDescription()
+    {
+
+        do                            // шукаємо <description>
+        xmlReader.Read();
+        while(xmlReader.Name != "description");            
+        xmlReader.Read();             // читаємо опис теста            
+        label1.Text = xmlReader.Value;// виводимо опис тесту        
+        xmlReader.Read();             // виходимо з  <description>  
+        do xmlReader.Read();          // шукаємо запитання по тегу <qw>
+        while (xmlReader.Name != "qw");        
+        xmlReader.Read();            // вмходимо з <qw>
+    }
+
+    // читаємо питання
+    private Boolean getQw() 
+    {
+        
+        xmlReader.Read();           // шуаємо <q>
+        if (xmlReader.Name == "q")
+        {
+            quest  = xmlReader.GetAttribute("text");  //присвоюємо змінним елементи( питання, картинку)
+            pic = xmlReader.GetAttribute("src");
+                       
+            xmlReader.Read();          // потрапляємо в простір з відповідями
+            int i = 0;            
+            while (xmlReader.Name != "q")
+            {
+                xmlReader.Read();              // зчитуємо елементи q <q>                
+                if (xmlReader.Name == "a")        // шукаємо за тегом варіанти відповідей
+                {
+                  if (xmlReader.GetAttribute("answer") == "yes")  // запам'ятовуємо правильну відповідь
+                  right = i;
+                        
+                  xmlReader.Read();          // зчитуємо варіанти відповідей в масив
+                  if (i < 4) answer[i] = xmlReader.Value;
+                    
+                    xmlReader.Read();          // виходимо з <a>
+                    i++;
+                }
+            }            
+            xmlReader.Read();      // виходимо з <q>
+            return true;
+        }
+        // на випадок якщо тег не є тегом питання <q>
+        else
+        return false;
+    }   
+    private void showQw()  // відображаємо питання та відповідь
+    {        
+        label1.Text = quest;      // виводимо питання        
+        if (pic.Length != 0)// відображення картинок
+        {           
+           pictureBox1.Image =
+           new Bitmap(pic);
+           pictureBox1.Visible = true;
+           radioButton1.Top = pictureBox1.Bottom + 16;           
+        }
+        else
+        {
+          if (pictureBox1.Visible)
+              pictureBox1.Visible = false;
+              radioButton1.Top = label1.Bottom;
+        }
+
+        // відображаємо в кнопках варіанти відповідей
+        radioButton1.Text = answer[0];
+        radioButton2.Top  = radioButton1.Top + 24;
+        radioButton2.Text = answer[1];
+        radioButton3.Top  = radioButton2.Top + 24;
+        radioButton3.Text = answer[2];
+        radioButton4.Top = radioButton3.Top + 24; 
+        radioButton4.Text = answer[3];
+
+        radioButton5.Checked = true; 
+        button1.Enabled = false;
+    }
+    
+    private void radioButton1_Click(object sender, EventArgs e) // функція бля обробки натиску на кнопки відповідей
+    {
+        if ((RadioButton)sender == radioButton1) choosequ = 0;
+        if ((RadioButton)sender == radioButton2) choosequ = 1;
+        if ((RadioButton)sender == radioButton3) choosequ = 2;
+        if ((RadioButton)sender == radioButton4) choosequ = 3;
+            button1.Enabled = true;
+    }
+        private void timer1_Tick(object sender, EventArgs e)  //таймер
+        {
+            
+            s = s - 1;
+            if (s == -1)//кінець хвилини
+            {
+                m = m - 1;
+                s = 59;
+            }
+
+            if (m == 0 && s == 0)//кінець часу
+            {
+                timer1.Stop();
+                MessageBox.Show("Час вийшов!");
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                pictureBox1.Visible = false;
+                if (sumrig == 12)
+                    d = "Відмінно ";
+                else if (sumrig <= 11 && sumrig >= 10)
+                    d = "Дуже добре";
+                else if (sumrig <= 9 && sumrig >= 7)
+                    d = "Добре";
+                else if (sumrig <= 6 && sumrig >= 5)
+                    d = "Задовільно";
+                else if (sumrig <= 4)
+                    d = "Незадовільно";
+                label1.Text = "Тестування закінчено.\n" +
+                              "Студент " + k.ToString() + " " + "курсу " + " " + "групи " +
+                               g.ToString() + " " + n.ToString() + ".\n" +
+                               "Правильно відповів на" + " " + sumrig.ToString() + " " +
+                               "з" + " " + sumque.ToString() + " " + "питань.\n " +
+                               "Ваш рівень знань: " + " " + d.ToString();
+            }
+            label2.Text = Convert.ToString(m + " :");//відображення часу в програмі
+            label3.Text = Convert.ToString(s);
+
+        }
+        
+       
+        private void button1_Click_1(object sender, EventArgs e) // натиск по кнопці ОК
+    {
+        switch (mode)
+        {
+        case 0:                         // режим початку роботи
+            n = textBox1N.Text;        // присваюємо змінним значення з тестбоксів
+            g = textBox2G.Text;
+            k = textBox3K.Text;           
+            m = 4;                      // хвилини та секунди таймера
+            s = 0;
+            timer1.Start();
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5N.Visible = false; 
+            label6G.Visible = false;
+            label7K.Visible = false;
+            textBox1N.Visible = false;
+            textBox2G.Visible = false;
+            textBox3K.Visible = false;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            radioButton3.Visible = true;
+            radioButton4.Visible = true;
+
+            this.getQw();
+            this.showQw();
+
+            mode = 1;
+
+            button1.Enabled = false;
+            radioButton5.Checked = true;
+            break;
+
+        case 1:
+            sumque++;   // лічильник кількості питань
+
+            if (choosequ == right) sumrig++; // перевірка на правильність відповіді
+
+            if (this.getQw()) this.showQw();
+            else                               // якщо питань більше немає 
+            {
+                
+                timer1.Stop();
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5N.Visible = false;
+                label6G.Visible = false;
+                label7K.Visible = false;
+                textBox1N.Visible = false;
+                textBox2G.Visible = false;
+                textBox3K.Visible = false;
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                pictureBox1.Visible  = false;
+                if (sumrig == sumque)
+                d = "Відмінно ";
+                else if (sumrig<=sumque-1 && sumrig>= sumque -2)
+                d = "Дуже добре";
+                else if (sumrig<=sumque-3 && sumrig>=sumque-5)
+                d = "Добре";
+                else if (sumrig <= sumque -6 && sumrig >= sumque - 7)
+                d = "Задовільно";
+                else if (sumrig <= sumque - 8 )
+                d = "Незадовільно";
+
+                label1.Text = "Тестування закінчено.\n" +
+                              "Студент " + k.ToString() +" "+ "курсу " + " " + "групи "+
+                               g.ToString() + " " + n.ToString() + ".\n" +
+                               "Правильно відповів на" + " " + sumrig.ToString() + " " + 
+                               "з" + " " + sumque.ToString() + " " + "питань.\n "+
+                               "Ваш рівень знань: "+" "+d.ToString();
+                               
+                                        
+                              
+                        
+
+                // следующий щелчок на кнопке Ok
+                // закроет окно программы
+                mode = 2;
+            }
+            break;
+            case 2:   // завершение работы программы
+            this.Close(); // закрыть окно
+            break;
+        }
+    }
+
+
+    }
+}
